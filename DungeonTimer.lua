627792d567251c488190147b2adfb7ae717351de
PProvost <peter@provost.org>
2010-01-10 19:18:51 -0700

    Cleaned up yucky formatting at the top

--- DungeonTimer.lua
+++ DungeonTimer.lua
@@ -16,22 +16,22 @@ Copyright 2009 Peter Provost (Quaiche of Dragonblight)
 
 local instanceInfo = {
 	-- Lich King 5-mans
-	["Ahn'kahet: The Old Kingdom"] 	= { type="bosskill", id=29311, },
-	["Azjol-Nerub"] 								= { type="bosskill", id=29120, },
-	["Drak'Tharon Keep"] 						= { type="bosskill", id=26632, },
-	["Gundrak"] 										= { type="bosskill", id=29306, },
-	["Halls of Lightning"] 					= { type="bosskill", id=28923 },
-	["Halls of Reflection"] 				= { type="emote", text="Nowhere to run... You're mine now!" },
-	["Halls of Stone"] 							= { type="bosskill", id=27978 },
-	["Pit of Saron"] 								= { type="bosskill", id=36658 },
-	["The Culling of Stratholme"] 	= { type="emote", text="Your journey has just begun" },
-	["The Forge of Souls"] 					= { type="bosskill", id=36502 },
-	["The Nexus"] 									= { type="bosskill", id=26723 },
-	["The Oculus"] 									= { type="bosskill", id=27656 },
-	["The Violet Hold"] 						= { type="bosskill", id=31134 },
-	["Trial of the Champion"] 			= { type="emote", text="No! I must not fail... again..." },
-	["Utgarde Keep"] 								= { type="emote", text="No! I can do... better! I can..." },
-	["Utgarde Pinnacle"] 						= { type="bosskill", id=26861 },
+	["Ahn'kahet: The Old Kingdom"] = { type="bosskill", id=29311, },
+	["Azjol-Nerub"] = { type="bosskill", id=29120, },
+	["Drak'Tharon Keep"] = { type="bosskill", id=26632, },
+	["Gundrak"] = { type="bosskill", id=29306, },
+	["Halls of Lightning"] = { type="bosskill", id=28923 },
+	["Halls of Reflection"] = { type="emote", text="Nowhere to run... You're mine now!" },
+	["Halls of Stone"] = { type="bosskill", id=27978 },
+	["Pit of Saron"] = { type="bosskill", id=36658 },
+	["The Culling of Stratholme"] = { type="emote", text="Your journey has just begun" },
+	["The Forge of Souls"] = { type="bosskill", id=36502 },
+	["The Nexus"] = { type="bosskill", id=26723 },
+	["The Oculus"] = { type="bosskill", id=27656 },
+	["The Violet Hold"] = { type="bosskill", id=31134 },
+	["Trial of the Champion"] = { type="emote", text="No! I must not fail... again..." },
+	["Utgarde Keep"] = { type="emote", text="No! I can do... better! I can..." },
+	["Utgarde Pinnacle"] = { type="bosskill", id=26861 },
 }
 
 -- Debug stuff
